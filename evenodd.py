e3d50bd3a7575c7707c95a36f28a8dc88ae69df3
Pallavi Borse <borsepallavi630@gmail.com>
2024-04-23 08:47:22 +0000

    this three new python codes

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+# Define a number
+number = 7
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(number, "is even.")
+else:
+    print(number, "is odd.")
+
---
+++ sum.py
@@ -0,0 +1,10 @@
+# Define two numbers
+num1 = 10
+num2 = 5
+
+# Calculate the sum
+sum = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum)
+
