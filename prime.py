5c465263d06f0ba4ee4953f3740fd73a83b70772
Pallavi Borse <borsepallavi630@gmail.com>
2024-04-23 09:04:48 +0000

    this is code for prime number

---
+++ prime.py
@@ -0,0 +1,24 @@
+# Function to check if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+# Input number from user
+num = int(input("Enter a number: "))
+
+# Check if the number is prime and print the result
+if is_prime(num):
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
+
